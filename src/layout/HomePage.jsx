a71b8afbf6e35fdcd4e6a8ab61958a0f13c3637d
Prince Chauhan <princecan1102@gmail.com>
2025-10-25 00:35:39 -0400

    Remove Text

--- src/layout/HomePage.jsx
+++ src/layout/HomePage.jsx
@@ -11,7 +11,6 @@ const HomePage = () => {
         <div className="space-y-6">
            <ImageCarousel   />
            {/* Add the other components. */}
-           <p className="w-full h-full text-red-600 text-2xl bg-gray-300 flex items-center justify-center">Other components to be added by Jay. Thank you.</p>
             {/* Slider Component */}
             <Slider />
             <DestinationSlider/>
