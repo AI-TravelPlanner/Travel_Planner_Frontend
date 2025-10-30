f0511876b7e1d520909dc547ecd621bad64185b1
Mohammed Nazamul Hassan <hassan.nazamul05@gmail.com>
2025-10-30 08:22:10 -0400

    localstorage is implemented to save the local changed

--- src/app/store.js
+++ src/app/store.js
@@ -6,15 +6,33 @@ import authReducer from "@/authSlice/authSlice"
 import tripReducer from '@/redux-slices/tripSlice'
 import tripFormReducer from '@/redux-slices/tripSearchSlice'
 
+
+const STORAGE_KEY = 'activeTripState';
+
 const store = configureStore({
     reducer: {
         auth: authReducer,
         boards: boardReducer,
         aliasTemplates: aliasTemplatesReducer,
         filter: filterReducer,
-        trips: tripReducer, 
+        trips: tripReducer,
         tripSearch: tripFormReducer
     },
 })
+
+
+// --- THIS IS THE AUTO-SAVE LOGIC ---
+// This function will run *after* every single action
+store.subscribe(() => {
+    try {
+        const state = store.getState();
+        // We only want to save the 'boards' slice
+        const serializedState = JSON.stringify(state.boards);
+        localStorage.setItem(STORAGE_KEY, serializedState);
+    } catch (err) {
+        console.error("Could not save state to localStorage", err);
+    }
+});
+// --- END OF AUTO-SAVE LOGIC ---
 
 export default store
